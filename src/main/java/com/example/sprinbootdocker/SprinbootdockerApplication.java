8d4a2122b4ef4729254f8ff422f3e2f6377dd637
sivaops7 <sivabalana91@gmail.com>
2023-06-29 23:39:12 +0530

    first commit

---
+++ src/main/java/com/example/sprinbootdocker/SprinbootdockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprinbootdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbootdockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinbootdockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sprinbootdocker/SprinbootdockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprinbootdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinbootdockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
